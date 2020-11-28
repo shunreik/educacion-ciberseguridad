797d97262e3df6ca534a804758c67261eae94e6d
shunreik <michu.arias17@hotmail.es>
2020-11-27 22:31:14 -0500

    Personalizando la vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,33 +16,45 @@
             @csrf
 
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-label for="email" value="Correo electrónico" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-label for="password" value="Contraseña" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <input id="remember_me" type="checkbox" class="form-checkbox" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600">Mantener iniciada sesión</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        ¿Olvidaste tu contraseña?
                     </a>
                 @endif
 
                 <x-jet-button class="ml-4">
-                    {{ __('Login') }}
+                    Ingresar
                 </x-jet-button>
             </div>
+
+            <x-jet-section-border />
+
+            <div class="flex">
+                <span class="flex-grow text-sm text-gray-600">Si eres nuevo puedes registrarte seleccionando la siguiente opción</span>
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="flex-none bg-green-600 px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white text-center uppercase hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">
+                    Crear cuenta
+                </a>
+            @endif
+            </div>
+            
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
