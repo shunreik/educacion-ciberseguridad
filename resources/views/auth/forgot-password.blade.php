11cfc6e937b632b21d905f57a22c18895f4ea6b0
shunreik <michu.arias17@hotmail.es>
2020-11-28 13:29:37 -0500

    Personalización de la vista de restablecimiento de contrasenia

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,8 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            ¿Olvidaste tu contraseña? No hay problema. Simplemente háganos saber su correo electrónico y le enviaremos un enlace de restablecimiento de contraseña.
+            {{-- {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }} --}}
         </div>
 
         @if (session('status'))
@@ -26,7 +27,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    Enviar enlace
+                    {{-- {{ __('Email Password Reset Link') }} --}}
                 </x-jet-button>
             </div>
         </form>
