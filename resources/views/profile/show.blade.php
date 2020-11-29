dfac22160f903d891077530c4cc0c0b7f6196cf9
shunreik <michu.arias17@hotmail.es>
2020-11-29 01:06:40 -0500

    Se descativó las funcionalidades de verificación de dos pasos y eliminar cuenta

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -36,11 +36,11 @@
                 @livewire('profile.logout-other-browser-sessions-form')
             </div>
 
-            <x-jet-section-border />
+            {{-- <x-jet-section-border /> --}}
 
-            <div class="mt-10 sm:mt-0">
+            {{-- <div class="mt-10 sm:mt-0">
                 @livewire('profile.delete-user-form')
-            </div>
+            </div> --}}
         </div>
     </div>
 </x-app-layout>
