f8600e42044c1e7f3301a9260a3018afab13f050
shunreik <michu.arias17@hotmail.es>
2020-12-29 13:50:01 -0500

    Aplicación de la opción para ver o ocultar contrasenia y traducciones de validaciones de password

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -2,7 +2,8 @@
 
 namespace App\Actions\Fortify;
 
-use Laravel\Fortify\Rules\Password;
+use App\Rules\Password as RulesPassword;
+// use Laravel\Fortify\Rules\Password;
 
 trait PasswordValidationRules
 {
@@ -13,6 +14,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed'];
+        return ['required', 'string', new RulesPassword, 'confirmed'];
     }
 }
