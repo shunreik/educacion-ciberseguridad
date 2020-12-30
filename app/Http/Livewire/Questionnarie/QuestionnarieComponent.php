bce883fea709a07756cb631ab31f3f7cb9c4d338
shunreik <michu.arias17@hotmail.es>
2020-12-29 20:11:57 -0500

    BÃuscador y filtros en el módulo de cuestionario

--- app/Http/Livewire/Questionnarie/QuestionnarieComponent.php
+++ app/Http/Livewire/Questionnarie/QuestionnarieComponent.php
@@ -3,9 +3,11 @@
 namespace App\Http\Livewire\Questionnarie;
 
 use App\Models\Questionnarie;
+use App\Models\User;
 use Livewire\Component;
 use Illuminate\Http\Request;
 use Livewire\WithPagination;
+use Illuminate\Database\Eloquent\Builder;
 
 class QuestionnarieComponent extends Component
 {
@@ -15,10 +17,23 @@ class QuestionnarieComponent extends Component
     public $confirmEditModal = false;
     public $view = 'confirmEdit';
 
+    //opciones de filtrado
+    public $all = true,
+        $actived = false,
+        $disabled = false;
+
+    //Opciones de búsqueda
+    public $search = '';
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+    ];
+
     public function render(Request $request)
     {
         $user = $request->user(); //se obtiene al usuario que está realizando la petición
-        $readings = $user->readings()->latest()->paginate(10);
+        $readings = $this->filterCuestionnaries($user);
+        // $readings = $user->readings()->latest()->paginate(10);
 
         return view('livewire.questionnarie.component', [
             'readings' => $readings,
@@ -44,5 +59,59 @@ class QuestionnarieComponent extends Component
         $questionnarie->save();
 
         return redirect()->route('questions', $this->readingId);
+    }
+
+    /**
+     * Método que permite listar a todos los cuestionarioes
+     */
+    public function allCuestionnaries()
+    {
+        $this->all = true;
+        $this->actived = false;
+        $this->disabled = false;
+    }
+
+    /**
+     * Método que permite el filtrado de cuestionarios publicados
+     */
+    public function activatedCuestionnaries()
+    {
+        $this->actived = true;
+        $this->disabled = false;
+        $this->all = false;
+    }
+
+    /**
+     * Método que permitr el filtrado de cuestionarios ocultos o desactivados
+     */
+    public function disabledCuestionnaries()
+    {
+        $this->disabled = true;
+        $this->actived = false;
+        $this->all = false;
+    }
+
+    public function filterCuestionnaries(User $user){
+        $readings = $user->readings();
+
+        if(!empty($this->search)){
+            $readings = $readings->where("title", 'LIKE', "%$this->search%");
+        }
+
+        if ($this->all) {
+            $readings = $readings;
+        }
+        if ($this->actived) {
+            $readings = $readings->whereHas('questionnarie', function (Builder $query) {
+                $query->where('status', true);
+            });
+        }
+        if ($this->disabled) {
+            $readings = $readings->whereHas('questionnarie', function (Builder $query) {
+                $query->where('status', false);
+            });
+        }
+        
+        return $readings->latest()->paginate(10);
     }
 }
--- resources/views/livewire/questionnarie/component.blade.php
+++ resources/views/livewire/questionnarie/component.blade.php
@@ -17,21 +17,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div class="mt-2 text-2xl">
-                        Cuestionarios registrados
+                    <div class="grid lg:grid-cols-2 gap-1">
+                        <div class="mt-2 text-2xl">
+                            Cuestionarios registrados
+                        </div>
+
+                        <div class="box my-4 lg:my-0">
+                            <div class="box-wrapper">
+                                <div class=" bg-white rounded-full flex items-center w-full p-3 shadow-sm border border-gray-200">
+                                  <div  class="outline-none focus:outline-none"><svg class=" w-5 text-gray-600 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></div>
+                                  <input type="search" name="" id="" wire:model="search" placeholder="Buscar lectura en base al título" class="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent">
+                                  
+                                  {{-- <div class="select">
+                                    <select name="" id="" wire:model="typeSearch" class="text-sm outline-none focus:outline-none bg-transparent mx-2">
+                                      <option value="title" selected>Título</option>
+                                      <option value="topic">Temática</option>
+                                     </select>
+                                  </div> --}}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="mt-4 text-gray-500 mb-2">
                         A continuación puedes agregar o editar el cuestionario en cada una de tus lecturas.
                     </div>
 
                     {{-- Opciones de filtrado --}}
-                    {{-- <div class="flex justify-center mb-4">
+                    <div class="flex justify-center mb-4">
                         <x-filters.status>
-                            <x-buttons.filter class="{{$all ? 'active' : ''}}" wire:click='allUsers'>Todos</x-buttons.filter>
-                            <x-buttons.filter color='green' class="{{$actived ? 'active' : ''}}" wire:click='activatedUsers'>Activos</x-buttons.filter>
-                            <x-buttons.filter color='pink' class="{{$disabled ? 'active' : ''}}" wire:click="disabledUsers">Inactivos</x-buttons.filter>
+                            <x-buttons.filter class="{{$all ? 'active' : ''}}" wire:click='allCuestionnaries'>Todos</x-buttons.filter>
+                            <x-buttons.filter color='green' class="{{$actived ? 'active' : ''}}" wire:click='activatedCuestionnaries'>Activos</x-buttons.filter>
+                            <x-buttons.filter color='pink' class="{{$disabled ? 'active' : ''}}" wire:click="disabledCuestionnaries">Inactivos</x-buttons.filter>
                         </x-filters.status>
-                    </div> --}}
+                    </div>
                    
                     <x-tables.questionnaries :data="$readings"/>
 
